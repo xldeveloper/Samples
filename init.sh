f6f6bc5133fbbc61dfe32e4336beb563b670e9ab
xldeveloper <xl.dev@laposte.net>
2014-09-21 23:08:11 +0700

    mod submodules init script (added some screen messages)

--- init.sh
+++ init.sh
@@ -3,14 +3,16 @@
 
 # TODO check if cleansed git local repo (to avoid to commit undesired files with the submodules)
 if [ ! -d Scripts ]; then 
+	echo "Adding 'Scripts' to repository && commit changes";
 	git submodule add https://github.com/xldeveloper/Scripts.git Scripts; 
 	git commit -m "Added 'Scripts' git sub-module"
 fi;
 #check if '.gitmodules' file exist (!0)
 gitmodulesfile=$(ls -a '.gitmodules' | sed -e "/\.$/d" | wc -l);
 if [ ! $gitmodulesfile -eq 0 ]; then
+	echo "Initializing & updating sub-modules";
 	git submodule init; 
 	git submodule update; 
 fi;
 
-git submodule update; 
+#git submodule update; 
