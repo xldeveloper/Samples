811f7006222749c2ef47af5e354c0fc6788f13f0
xldeveloper <xl.dev@laposte.net>
2014-09-21 21:56:19 +0700

    modified submodules init script

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+
+# TODO check if cleansed git local repo (to avoid to commit undesired files with the submodules)
 if [ ! -d Scripts ]; then 
 	git submodule add https://github.com/xldeveloper/Scripts.git Scripts; 
 	git submodule init; 
