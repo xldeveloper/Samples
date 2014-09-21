b7034edc795b66a8d9aa65dd5a4d37841207f665
xldeveloper <xl.dev@laposte.net>
2014-09-21 21:52:33 +0700

    Modified submodules init script

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ if [ ! -d Scripts ]; then
 	git submodule add https://github.com/xldeveloper/Scripts.git Scripts; 
 	git submodule init; 
 	git submodule update; 
+	git commit -m "Added 'Scripts' git sub-module"
 fi;
 git submodule update; 
