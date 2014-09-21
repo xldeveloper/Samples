5ff09f38e8263065aeefd4354a426d3628eb3855
xldeveloper <xl.dev@laposte.net>
2014-09-21 23:03:54 +0700

    fixed oops on modified submodules init/deinit scripts

--- init.sh
+++ init.sh
@@ -3,16 +3,14 @@
 
 # TODO check if cleansed git local repo (to avoid to commit undesired files with the submodules)
 if [ ! -d Scripts ]; then 
-	echo "add.";
-	#git submodule add https://github.com/xldeveloper/Scripts.git Scripts; 
-	#git commit -m "Added 'Scripts' git sub-module"
+	git submodule add https://github.com/xldeveloper/Scripts.git Scripts; 
+	git commit -m "Added 'Scripts' git sub-module"
 fi;
 #check if '.gitmodules' file exist (!0)
 gitmodulesfile=$(ls -a '.gitmodules' | sed -e "/\.$/d" | wc -l);
 if [ ! $gitmodulesfile -eq 0 ]; then
-	echo "init.";
-	#git submodule init; 
-	#git submodule update; 
+	git submodule init; 
+	git submodule update; 
 fi;
 
-#git submodule update; 
+git submodule update; 
