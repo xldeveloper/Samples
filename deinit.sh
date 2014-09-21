7a3c4948cb747dbb6aa1bfd079f03620837b0ba3
xldeveloper <xl.dev@laposte.net>
2014-09-21 22:28:00 +0700

    modified submodules deinit script to clean unused system files

--- deinit.sh
+++ deinit.sh
@@ -15,6 +15,16 @@ if [ -d Scripts ]; then
 		git rm Scripts;
 		rm -rf '.git/modules/Scripts';
 		git commit -m "Removed 'Scripts' git sub-module"
+
+		# check if '.git/modules' dir is empty (0)
+		modulesdircontent=$(ls -a '.git/modules' | sed -e "/\.$/d" | wc -l);
+		#check if '.gitmodules' file exist (!0)
+		gitmodulesfile=$(ls -a '.gitmodules' | sed -e "/\.$/d" | wc -l);
+		if [ $modulesdircontent -eq 0 ] && [ ! $gitmodulesfile -eq 0 ];
+		then
+			git rm '.gitmodules';
+		fi;
+		
 	else
 		error_exit "Cannot remove sub-module 'Scripts'";
 	fi;
