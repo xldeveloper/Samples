5a20c7d774f55981ed2e7790541049c793c97579
xldeveloper <xl.dev@laposte.net>
2014-09-21 23:34:36 +0700

    mod submod init/deinit scripts

--- deinit.sh
+++ deinit.sh
@@ -19,7 +19,7 @@ if [ -d Scripts ]; then
 		# check if '.git/modules' dir is empty (0)
 		modulesdircontent=$(ls -a '.git/modules' | sed -e "/\.$/d" | wc -l);
 		#check if '.gitmodules' file exist (!0)
-		gitmodulesfile=$(ls -a '.gitmodules' | sed -e "/\.$/d" | wc -l);
+		gitmodulesfile=$(ls -a '.gitmodules' 2>/dev/null | sed -e "/\.$/d" | wc -l);
 		if [ $modulesdircontent -eq 0 ] && [ ! $gitmodulesfile -eq 0 ]; then
 			git rm '.gitmodules';
 			git commit -m "Removed useless '.gitmodules' file"
--- init.sh
+++ init.sh
@@ -4,15 +4,14 @@
 # TODO check if cleansed git local repo (to avoid to commit undesired files with the submodules)
 if [ ! -d Scripts ]; then 
 	echo "Adding 'Scripts' to repository && commit changes";
-	git submodule add https://github.com/xldeveloper/Scripts.git Scripts; 
-	git commit -m "Added 'Scripts' git sub-module"
+	#git submodule add https://github.com/xldeveloper/Scripts.git Scripts && git commit -m "Added 'Scripts' git sub-module";
 fi;
 #check if '.gitmodules' file exist (!0)
-gitmodulesfile=$(ls -a '.gitmodules' | sed -e "/\.$/d" | wc -l);
+gitmodulesfile=$(ls -a '.gitmodules' 2>/dev/null | sed -e "/\.$/d" | wc -l);
 if [ ! $gitmodulesfile -eq 0 ]; then
 	echo "Initializing & updating sub-modules";
-	git submodule init; 
-	git submodule update; 
+	#git submodule init; 
+	#git submodule update; 
 fi;
 
 #git submodule update; 
