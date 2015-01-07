f9887842908c7662eeaa1020ee5fc4460bc065de
xldeveloper <xl.dev@laposte.net>
2015-01-07 16:38:58 +0700

    modified init bash script

--- init.sh
+++ init.sh
@@ -5,18 +5,22 @@
 if [ ! -d Scripts ]; then 
 
 	echo "Adding 'Scripts' to repository && commit changes";
-	git submodule add https://github.com/xldeveloper/Scripts.git Scripts && git commit -m "Added 'Scripts' git sub-module" && git submodule init && git submodule update;
+	git submodule add https://github.com/xldeveloper/Scripts.git Scripts \
+	&& git commit -am "Added 'Scripts' git sub-module" \
+	&& git submodule init \
+	&& git submodule update;
 
 	# add other sub-modules
 	if [ -d Scripts ]; then 
 		./Scripts/git-add-submodule-pairs.sh "https://github.com/xldeveloper/Sample2D.git" "Sample2D" "https://github.com/xldeveloper/Sample3D.git" "Sample3D";
-		git commit -m "Added sub-modules";
+		git commit -am "Added sub-modules";
 	fi;
 fi;
 #check if '.gitmodules' file exist (!0)
 gitmodulesfile=$(ls -a '.gitmodules' 2>/dev/null | sed -e "/\.$/d" | wc -l);
 if [ ! $gitmodulesfile -eq 0 ]; then
 	echo "Initializing & updating sub-modules";
-	git submodule init && git submodule update; 
+	git submodule init \
+	&& git submodule update --init --recursive \
+	&& git submodule update --remote --rebase --recursive; 
 fi;
-
